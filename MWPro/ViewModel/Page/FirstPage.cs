ff8d4f44028c29b44d9abf4749eb38a223b2ac2a
adelvig-an <63202963+adelvig-an@users.noreply.github.com>
2021-04-24 21:11:45 +0700

    Обновление

--- MWPro/ViewModel/Page/FirstPage.cs
+++ MWPro/ViewModel/Page/FirstPage.cs
@@ -1,6 +1,28 @@
-﻿namespace MWPro.ViewModel.Page
+﻿using MWPro.ViewModel.Base;
+using System.Windows.Input;
+
+namespace MWPro.ViewModel.Page
 {
     public class FirstPage : PageViewModel
     {
+        private ICommand showDialogAction;
+
+        public ICommand ShowDialogAction
+        {
+            get
+            {
+                if (showDialogAction == null)
+                {
+                    showDialogAction = new RelayCommand(() => 
+                    { }, () => { return true; });
+                }
+                return showDialogAction;
+            }
+        }
+
+        public FirstPage()
+        {
+            
+        }
     }
 }
